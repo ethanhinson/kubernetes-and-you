e6e7ee135a4a9b9586adb9dc1ffad9acaafe4cb5
ethanhinson <ethan@bluetent.com>
2023-11-20 20:47:06 -0700

    Refactor ingress-nginx taints for control plane, add 2 worker nodes to the cluster

--- bin/3.install-ingress-nginx.sh
+++ bin/3.install-ingress-nginx.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 # https://kubernetes.github.io/ingress-nginx/deploy/
-helm upgrade --install ingress-nginx ingress-nginx \
-  --repo https://kubernetes.github.io/ingress-nginx \
-  --namespace ingress-nginx --create-namespace
+
+helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
+helm repo update ingress-nginx
+helm install --kube-context kind-kind -n ingress-nginx --create-namespace \
+  --values https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/hack/manifest-templates/provider/kind/values.yaml \
+  --set controller.allowSnippetAnnotations=true ingress-nginx ingress-nginx/ingress-nginx
+kubectl --context kind-kind -n ingress-nginx wait \
+  --timeout=300s \
+  --for=condition=ready pod \
+  --selector=app.kubernetes.io/component=controller
+
+echo "Ingress-Nginx is ready!"
